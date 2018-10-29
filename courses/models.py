0f3921b6bef93777e64800aee8a2d83286b2d352
jamshaid <jamshaid1824@gmail.com>
2018-10-29 12:30:54 +0500

    youtube video embed

--- courses/models.py
+++ courses/models.py
@@ -4,6 +4,16 @@ from django.db import models
 from django.db import models
 
 # Create your models here.
+VIDEO = 'video'
+IMAGE = 'image'
+YOUTUBE = 'youtube'
+MATERIAL_CHOICES = (
+
+        (VIDEO, 'VIDEO'),
+        (IMAGE, 'IMAGE'),
+        (YOUTUBE, 'YOUTUBE'),
+
+    )
 class Course(models.Model):
     title = models.CharField(max_length=500)
     description=models.TextField()
@@ -15,6 +25,9 @@ class Course(models.Model):
     eligibility = models.TextField()
     courseoutline = models.TextField()
     background_image = models.ImageField(upload_to='courses_imgs/')
+    material_type = models.CharField(max_length=20, choices=MATERIAL_CHOICES, default=IMAGE)
+    material = models.FileField(upload_to='helpingmatrial/', blank=True)
+    url = models.CharField(max_length=150, blank=True)
     created_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
